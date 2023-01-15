abd40afd51151bb1a2db45cab89d94e904aa29a4
jongseok-lee <jongseok.lee@linecorp.com>
2023-01-15 16:59:58 +0900

    docker reverse proxy 설정

--- be/src/server.js
+++ be/src/server.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.json());)
+
 app.get("/", (req, res) => {
-  res.send("Hello World! Welcome to Tamastudy :)");
+  res.status(200).json("Hello World! Welcome to Tamastudy :)");
+});
+
+app.get("/v1/hello", (req, res) => {
+  res.status(200).json("v1 hello");
 });
 
 app.listen(port, () => {
