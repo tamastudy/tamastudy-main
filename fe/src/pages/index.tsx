490cf042ed6e89f6db58002da0c7580e156b315c
jongseok-lee <jongseok.lee@linecorp.com>
2023-01-17 13:47:40 +0900

    member component

--- fe/src/pages/index.tsx
+++ fe/src/pages/index.tsx
@@ -21,8 +21,18 @@ import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 
 const SCROLL_LINK_OFFSET = -64 - 16;
 
-function shuffle<T>(array: T[]): T[] {
-  return array.sort(() => Math.random() - 0.5);
+function shuffle<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 export const getStaticProps: GetStaticProps<{ users: User[] }> = async () => {
