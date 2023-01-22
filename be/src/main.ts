d8fd84e3d59e776387b2b7dee28b8dd500b8d82b
jongseok-lee-radarlab <jongseok@radar-lab.com>
2023-01-22 14:41:41 +0900

    depends on

--- be/src/main.ts
+++ be/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({}));
   app.enableCors();
-  await app.listen(6000);
+  await app.listen(8080);
 }
 bootstrap();
