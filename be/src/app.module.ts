e45b02131d5fc4b78d34de4512c994cbb58aea3d
jongseok-lee-radarlab <jongseok@radar-lab.com>
2023-01-22 14:12:19 +0900

    docker network

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -14,7 +14,7 @@ console.log({ TFA_APP_NAME: process.env.TFA_APP_NAME });
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.NODE_ENV === 'production' ? 'db' : 'localhost',
       port: 5432,
       username: 'postgres',
       password: 'pass123',
